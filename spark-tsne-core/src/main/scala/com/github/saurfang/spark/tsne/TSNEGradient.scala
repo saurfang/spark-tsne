6dd804f1942b9aa7b581b3a310b5e5096f0017dc
Forest Fang <forest.fang@outlook.com>
2015-07-17 23:42:05 -0400

    remove dead code
    Former-commit-id: 4c94108e284ce00a898270dfa42e1428bcf86730

--- spark-tsne-core/src/main/scala/com/github/saurfang/spark/tsne/TSNEGradient.scala
+++ spark-tsne-core/src/main/scala/com/github/saurfang/spark/tsne/TSNEGradient.scala
@@ -20,20 +20,6 @@ object TSNEGradient extends Logging  {
     val num: DenseMatrix[Double] = (y1(::, *) + sumY).t
     num := 1.0 :/ (1.0 :+ (num(::, *) + sumY(i).toDenseVector))
 
-//
-//    val y1: DenseMatrix[Double] = -2.0 :* (Y * Y.t)
-//    val y2 = (y1(::, *) + sumY).t
-//    val num = 1.0 :/ (1.0 :+ (y2(::, *) + sumY))
-
-    // Y_diff = Y_i - Y
-//    val yDiff: DenseMatrix[Double] = Y(*, ::) - Y(i, ::).inner
-    // Y_sum = ||Y_i - Y||^2
-//    val num = sum((yDiff :* yDiff).apply(*, ::))
-    //val ySquared = Y :* Y
-    //val num = ySquared(i) :- (2 :* Y(i) :* Y) :+ ySquared
-    // num = (1 + ||Y_i - Y||^2)^-1
-    // num := 1.0 :/ (1.0 :+ num)
-    //diag(num) := DenseVector.zeros[Double](i.length)
     i.indices.foreach(idx => num.unsafeUpdate(idx, i(idx), 0.0))
     num := num.mapValues(math.max(_, 1e-12))
 
@@ -57,20 +43,6 @@ object TSNEGradient extends Logging  {
                totalNum: Double,
                dY: DenseMatrix[Double],
                exaggeration: Boolean): Double = {
-    //val n = Y.rows
-    //val m = data.length
-//    val p = {
-//      val mat = DenseMatrix.zeros[Double](m, n)
-//      data.zipWithIndex.foreach {
-//        case ((_, itr), i) =>
-//          itr.foreach {
-//            case (j, v) =>
-//              mat.unsafeUpdate(i, j, math.max(if(exaggeration) v * 4 else v, 1e-12))
-//          }
-//      }
-//      mat
-//    }
-
     // q = (1 + ||Y_i - Y_j||^2)^-1 / sum(1 + ||Y_k - Y_l||^2)^-1
     val q: DenseMatrix[Double] = num :/ totalNum
 
@@ -89,20 +61,13 @@ object TSNEGradient extends Logging  {
     // l = [ (p_ij - q_ij) * (1 + ||Y_i - Y_j||^2)^-1 ]
     q := -q :* num
     // l_sum = [0 0 ... sum(l) ... 0]
-    //val sumL = DenseMatrix.zeros[Double](m, n)
     sum(q(*, ::)).foreachPair{ case (i, v) => q.unsafeUpdate(i, data(i)._1, q(i, i) - v) }
-//    logInfo(s"l : ${l.rows} x ${l.cols}")
-//    logInfo(s"sumL : ${sumL.rows} x ${sumL.cols}")
-//    logInfo(s"Y : ${Y.rows} x ${Y.cols}")
 
     // TODO: dY_i = 4 * (l_sum - l) * Y
     val dYi: DenseMatrix[Double] = -4.0 :* (q * Y)
-    //logInfo(s"dY before: $dY")
-    //logInfo(s"dYi: $dYi")
     data.map(_._1).zipWithIndex.foreach{
       case (i, idx) => dY(i, ::) := dYi(idx, ::)
     }
-    //logInfo(s"dY after: $dY")
 
     loss
   }
