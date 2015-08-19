5ef411e5607240efb3fec120b87227c3ab3f5a1a
Forest Fang <forest.fang@outlook.com>
2015-08-19 11:32:50 -0400

    correct P-Q

--- spark-tsne-core/src/main/scala/com/github/saurfang/spark/tsne/TSNEGradient.scala
+++ spark-tsne-core/src/main/scala/com/github/saurfang/spark/tsne/TSNEGradient.scala
@@ -13,17 +13,14 @@ object TSNEGradient extends Logging  {
    */
   def computeNumerator(Y: DenseMatrix[Double], idx: Int *): DenseMatrix[Double] = {
     // Y_sum = ||Y_i||^2
-    //logInfo(s"i: ${i.toList} \n Y: $Y")
-    val sumY = sum((Y :* Y).apply(*, ::)) // n * 1
+    val sumY = sum(pow(Y, 2).apply(*, ::)) // n * 1
     val subY = Y(idx, ::).toDenseMatrix // k * 1
     val y1: DenseMatrix[Double] = Y * (-2.0 :* subY.t) // n * k
     val num: DenseMatrix[Double] = (y1(::, *) + sumY).t // k * n
     num := 1.0 :/ (1.0 :+ (num(::, *) + sumY(idx).toDenseVector)) // k * n
 
-    idx.indices.foreach(i => num.unsafeUpdate(i, idx(i), 0.0))
-    //num.foreachPair{case ((i, j), v) => num.unsafeUpdate(i, j, math.max(v, 1e-12))}
+    idx.indices.foreach(i => num.unsafeUpdate(i, idx(i), 0.0)) // num(i, i) = 0
 
-    //logInfo(s"num: $num")
     num
   }
 
@@ -44,17 +41,18 @@ object TSNEGradient extends Logging  {
                dY: DenseMatrix[Double],
                exaggeration: Boolean): Double = {
     // q = (1 + ||Y_i - Y_j||^2)^-1 / sum(1 + ||Y_k - Y_l||^2)^-1
-    val q: DenseMatrix[Double] = num :/ totalNum
+    val q: DenseMatrix[Double] = num / totalNum
     q.foreachPair{case ((i, j), v) => q.unsafeUpdate(i, j, math.max(v, 1e-12))}
 
-    val loss = data.zipWithIndex.flatMap{
+    // q = q - p
+    val loss = data.zipWithIndex.flatMap {
       case ((_, itr), i) =>
         itr.map{
-          case (j, v) =>
-            val exaggeratedV = if(exaggeration) v * 4 else v
+          case (j, p) =>
+            val exaggeratedP = if(exaggeration) p * 4 else p
             val qij = q(i, j)
-            val l = exaggeratedV * math.log(exaggeratedV / qij)
-            q.unsafeUpdate(i, j,  qij - exaggeratedV)
+            val l = exaggeratedP * math.log(exaggeratedP / qij)
+            q.unsafeUpdate(i, j,  qij - exaggeratedP)
             if(l.isNaN) 0.0 else l
         }
     }.sum
@@ -62,7 +60,7 @@ object TSNEGradient extends Logging  {
     // l = [ (p_ij - q_ij) * (1 + ||Y_i - Y_j||^2)^-1 ]
     q :*= -num
     // l_sum = [0 0 ... sum(l) ... 0]
-    sum(q(*, ::)).foreachPair{ case (i, v) => q.unsafeUpdate(i, data(i)._1, q(i, i) - v) }
+    sum(q(*, ::)).foreachPair{ case (i, v) => q.unsafeUpdate(i, data(i)._1, q(i, data(i)._1) - v) }
 
     // dY_i = -4 * (l - l_sum) * Y
     val dYi: DenseMatrix[Double] = -4.0 :* (q * Y)
